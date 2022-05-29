0316dbe0bccbe1aab6d501e5cc6d3014b9d839fe
MajaPetkova <97319041+MajaPetkova@users.noreply.github.com>
2022-05-29 20:29:40 +0200

    Add search

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,12 @@
   const router = require('express').Router();
-  const cubes = require('../db.json');
+  //   const cubes = require('../db.json');
+  const cubeService = require('../services/cubeService')
 
 
   router.get('/', (req, res) => {
+      let { search, from, to } = req.query;
+      //   console.log(search, from, to)
+      const cubes = cubeService.getAll(search, from, to);
       res.render('index', { cubes })
   });
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -2,11 +2,18 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const cubes = require('../db.json');
+exports.getAll = (search = '', from = 0, to = 6) => {
+    const result = cubes
+        .filter(x => x.name.toLowerCase().includes(search))
+        .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to)
+
+    return result;
+};
+exports.getOne = (cubeId) => cubes[cubeId];
 
 exports.save = (cube) => {
     cubes.push({ id: cubes[cubes.length - 1].id + 1, ...cube });
     let textData = JSON.stringify('cubes', '', 4)
 
-    return fs.writeFile(path.resolve('src', 'db.json'), textData)
+    return fs.writeFile(path.resolve('src', 'db.json'), textData, { encoding: 'utf-8' })
 };
-exports.getOne = (cubeId) => cubes[cubeId]
