ec389b817f1c0f104b163c38ad7d5f042be316be
MajaPetkova <97319041+MajaPetkova@users.noreply.github.com>
2022-06-08 10:20:49 +0200

    create Cube Model

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
+
+const { initializedatabase } = require('./config/database')
+
 const routes = require('./routes');
 
 const app = express();
-
-
+require('./config/handlebars')(app);
 app.use('/static', express.static('public'));
 app.use(express.urlencoded({ extended: false }))
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs')
-app.set('views', './src/views')
+app.use(routes);
 
-app.use(routes)
+initializedatabase()
+    .then(() => {
+        app.listen(5000, () => console.log('App is started on port 5000'))
 
-app.listen(5000, () => console.log('App is started on port 5000'))
+    })
+    .catch((err) => {
+        console.log('Can not conect to db:', err)
+    })
